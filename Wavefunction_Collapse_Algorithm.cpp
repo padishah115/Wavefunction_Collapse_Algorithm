a238adecb4be5cc125172b8cb2f2da52be839002
hayde <hayde@LAPTOP-52LE53OM>
2023-07-11 12:43:40 +0100

    Second push

--- Wavefunction_Collapse_Algorithm.cpp
+++ Wavefunction_Collapse_Algorithm.cpp
@@ -6,6 +6,17 @@ const int x_max = 100;
 const int y_max = 100;
 int square_grid[x_max][y_max] = {0};
 
+//Collapse cell fxn- checks to see if tile is nonzero
+int collapse_cell(int x, int y) {
+	if (square_grid[x][y] != 0) {
+		return square_grid[x][y];
+	}
+	else {
+		get_possible_tiles[x][y];
+	}
+}
+
+//Gives tiles that could be neighbors
 void get_possible_tiles(int x, int y) {
 	if(x > 0){ 
 		//Append one to the left
