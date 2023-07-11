0a269508c7005afc06775877fd4953a68a092c29
hayde <hayde@LAPTOP-52LE53OM>
2023-07-11 12:34:30 +0100

    main algorithm source code

--- Wavefunction_Collapse_Algorithm.cpp
+++ Wavefunction_Collapse_Algorithm.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+
+//Define the grid as 100x100
+const int x_max = 100;
+const int y_max = 100;
+int square_grid[x_max][y_max] = {0};
+
+void get_possible_tiles(int x, int y) {
+	if(x > 0){ 
+		//Append one to the left
+		
+	}
+
+	if (x < x_max) {
+		//Append one to the right
+	}
+
+	if (y > 0) {
+		//Append one above
+	}
+
+	if (y < y_max) {
+		//Append one below
+	}
+}
+
 int main() {
+	
 
 
 	return 0;
